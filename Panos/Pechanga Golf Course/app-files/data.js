864a9aa2d1fbb3b8af5b9f398c05bbe7db764150
cheap-sunglasses <cheapsunglasses@spectrum.net>
2025-12-11 10:40:02 -0800

    Added Pechanga Golf Course

---
+++ Panos/Pechanga Golf Course/app-files/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pechanga-golf-course-photosphere",
+      "name": "Pechanga Golf Course Photosphere",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": -0.6625741718313449,
+        "pitch": 0.507704186015058,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

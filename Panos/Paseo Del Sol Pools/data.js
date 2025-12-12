945122aa761e7a4b238a691dd0eb81a1da5d81b3
cheap-sunglasses <cheapsunglasses@spectrum.net>
2025-12-12 11:51:31 -0800

    Added Paseo Del Pools

---
+++ Panos/Paseo Del Sol Pools/data.js
@@ -0,0 +1,95 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-paseo-del-pool---leena-way",
+      "name": "Paseo Del Pool - Leena Way",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": -2.9018612875860708,
+        "pitch": 1.0771212939187205,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8786187574156674,
+          "pitch": 0.09939215086072295,
+          "rotation": 0,
+          "target": "1-sunny-meadows-pool---paseo-del-sol"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sunny-meadows-pool---paseo-del-sol",
+      "name": "Sunny Meadows Pool - Paseo Del Sol",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 1.7369548218104693,
+        "pitch": 1.4656380958740165,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.020818771804482,
+          "pitch": 0.3416250684926947,
+          "rotation": 0,
+          "target": "0-paseo-del-pool---leena-way"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

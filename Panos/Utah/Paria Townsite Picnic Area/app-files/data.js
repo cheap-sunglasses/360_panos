7d1ae0e5cc1938bebeb78d8dbbadad7f64b15cab
cheap-sunglasses <cheapsunglasses@spectrum.net>
2025-12-13 18:42:10 -0800

    Added Paria Townsite Picnic Area

---
+++ Panos/Utah/Paria Townsite Picnic Area/app-files/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-paria-townsite-picnic-area",
+      "name": "Paria Townsite Picnic Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0.18552515660752178,
+        "pitch": 0.32844051165405475,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

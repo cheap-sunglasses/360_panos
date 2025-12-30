26255dceb1cfe1e06fb95d0046660d4c9024779b
cheap-sunglasses <cheapsunglasses@spectrum.net>
2025-12-29 17:00:32 -0800

    Added Galleron Park

---
+++ Panos/Galleron Park/app-files/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-galleron-park",
+      "name": "Galleron Park",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3250,
+      "initialViewParameters": {
+        "yaw": -1.4417921281968642,
+        "pitch": 0.3067224341860033,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

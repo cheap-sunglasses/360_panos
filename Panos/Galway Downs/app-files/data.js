ee92351b2f15a79592a6f91845148e9478bef6db
cheap-sunglasses <cheapsunglasses@spectrum.net>
2026-01-10 08:25:05 -0800

    Added Galway Downs

---
+++ Panos/Galway Downs/app-files/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-galway-downs",
+      "name": "Galway Downs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.2879594514983967,
+        "pitch": 0.3605492234282348,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

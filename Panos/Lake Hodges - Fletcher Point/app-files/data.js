81420c2d5b8243fc63f4d33c45357a355228e973
cheap-sunglasses <cheapsunglasses@spectrum.net>
2026-01-28 06:55:46 -0800

    Added Lake Hodges - Fletcher Point

---
+++ Panos/Lake Hodges - Fletcher Point/app-files/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lake-hodges---fletcher-point",
+      "name": "Lake Hodges - Fletcher Point",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3750,
+      "initialViewParameters": {
+        "yaw": -2.921849451712351,
+        "pitch": 0.2882642556871673,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

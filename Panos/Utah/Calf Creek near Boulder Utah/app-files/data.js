ff1356f69b331c7332877750b6e49e130d68a7ac
cheap-sunglasses <cheapsunglasses@spectrum.net>
2026-05-21 18:19:51 -0700

    Added Calf Creek near Boulder Utah

---
+++ Panos/Utah/Calf Creek near Boulder Utah/app-files/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-calf-creek-near-boulder-utah",
+      "name": "Calf Creek near Boulder Utah",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.2820548747525926,
+        "pitch": 0.45285458165774983,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

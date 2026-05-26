6b63849e40d95fc0fd6778a3c5e8e91c03097358
cheap-sunglasses <cheapsunglasses@spectrum.net>
2026-05-26 13:41:33 -0700

    Added Afton Canyon Campground

---
+++ Panos/Afton Canyon Campground/app-files/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-afton-canyon-campground",
+      "name": "Afton Canyon Campground",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.5776766023667115,
+        "pitch": 0.28698507163052156,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

38136aa30eed021e25e27203765ccc5d1db06c17
cheap-sunglasses <cheapsunglasses@spectrum.net>
2026-08-25 14:00:43 -0700

    added third tyee lake

---
+++ Panos/Central California/Third Tyee Lake/app-files/data.js
@@ -0,0 +1,46 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-third-tyee-lake",
+      "name": "Third Tyee Lake",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3490,
+      "initialViewParameters": {
+        "yaw": 1.0697708383733655,
+        "pitch": -0.06070722683802643,
+        "fov": 1.3926760049349705
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
